8b395338f588c5592b5a95fca923faff6a075d0b
jsommers <jsommers@colgate.edu>
2023-11-10 10:12:21 -0500

    turbostreams addition

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    # inside model class
+    after_commit :broadcast_post
+
+protected
+ def broadcast_post
+   broadcast_replace_later_to 'posts', partial: 'posts/post'
+   # can leave off target and partial if you abide by rails naming conventions...
+ end
 end
